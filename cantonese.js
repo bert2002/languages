c8aa0b03eda77ea3095b99e8d2653b43964d88da
Steffen Wirth <s.wirth@itbert.de>
2019-07-31 15:45:49 +0800

    add cantonese

---
+++ cantonese.js
@@ -0,0 +1,287 @@
+[
+	{
+		"lang1": "Hallo",
+		"lang2": "nei5 hou2",
+		"other": "你好"
+	},{
+		"lang1": "My Name is...",
+		"lang2": "ngo5 giu3...",
+		"other": "我叫..."
+	},{
+		"lang1": "What is your name?",
+		"lang2": "nei5 giu3 mat1 je5 mlang1g2 aa3?",
+		"other": ""
+	},{
+		"lang1": "I",
+		"lang2": "ngo5",
+		"other": ""
+	},{
+		"lang1": "Good",
+		"lang2": "hou2",
+		"other": ""
+	},{
+		"lang1": "Good Morning",
+		"lang2": "zou2 san4",
+		"other": ""
+	},{
+		"lang1": "Sorry",
+		"lang2": "dui3 m4 zyu6",
+		"other": ""
+	},{
+		"lang1": "You're welcome",
+		"lang2": "m4 sai2 haak3 hei3",
+		"other": ""
+	},{
+		"lang1": "Go",
+		"lang2": "heoi3",
+		"other": ""
+	},{
+		"lang1": "Know",
+		"lang2": "sik1",
+		"other": ""
+	},{
+		"lang1": "Listlang1",
+		"lang2": "tlang1g1",
+		"other": ""
+	},{
+		"lang1": "Have",
+		"lang2": "jau5",
+		"other": ""
+	},{
+		"lang1": "Drink",
+		"lang2": "jam2",
+		"other": ""
+	},{
+		"lang1": "How",
+		"lang2": "dim2 (+Verb)",
+		"other": ""
+	},{
+		"lang1": "You",
+		"lang2": "nei5",
+		"other": ""
+	},{
+		"lang1": "Name",
+		"lang2": "mlang1g2",
+		"other": ""
+	},{
+		"lang1": "Good afternoon",
+		"lang2": "ng5 on1",
+		"other": ""
+	},{
+		"lang1": "Good bye",
+		"lang2": "zoi3 gin3",
+		"other": ""
+	},{
+		"lang1": "Thank you",
+		"lang2": "do1 ze6",
+		"other": ""
+	},{
+		"lang1": "It's all right",
+		"lang2": "m4 gan2 jiu3",
+		"other": ""
+	},{
+		"lang1": "Help!",
+		"lang2": "gau3 mlang1g6",
+		"other": ""
+	},{
+		"lang1": "Go back",
+		"lang2": "faan1",
+		"other": ""
+	},{
+		"lang1": "Home",
+		"lang2": "uk1 kei2",
+		"other": ""
+	},{
+		"lang1": "Is not",
+		"lang2": "m4 hai6",
+		"other": ""
+	},{
+		"lang1": "Understand",
+		"lang2": "ming4 baak6",
+		"other": ""
+	},{
+		"lang1": "Speak",
+		"lang2": "gong2",
+		"other": ""
+	},{
+		"lang1": "Don't have",
+		"lang2": "mou5",
+		"other": ""
+	},{
+		"lang1": "Eat",
+		"lang2": "sik6",
+		"other": ""
+	},{
+		"lang1": "What",
+		"lang2": "mat1 je5",
+		"other": ""
+	},{
+		"lang1": "Go to work",
+		"lang2": "faan1 gong2",
+		"other": ""
+	},{
+		"lang1": "Excuse me (sorry)",
+		"lang2": "m4 hou2 ji3 si3",
+		"other": ""
+	},{
+		"lang1": "Get off Minibus",
+		"lang2": "m4 goi1, jau5 lok6",
+		"other": ""
+	},{
+		"lang1": "Not yet",
+		"lang2": "mei6 aa3",
+		"other": ""
+	},{
+		"lang1": "Have you eatlang1 yet?",
+		"lang2": "nei5 sik6 zo5 faan6 mei6 aa3?",
+		"other": ""
+	},{
+		"lang1": "What is it?",
+		"lang2": "mat1 je5 lai4 gaa3?",
+		"other": ""
+	},{
+		"lang1": "Food",
+		"lang2": "sik6 mat6",
+		"other": ""
+	},{
+		"lang1": "Do you speak Cantonese?",
+		"lang2": "nei5 sik1 m4 sik1 gong2 gwong2 dung1 waa2 aa3?",
+		"other": ""
+	},{
+		"lang1": "preslang1t",
+		"lang2": "lai5 mat6",
+		"other": ""
+	},{
+		"lang1": "litte, litte",
+		"lang2": "siu2 siu2",
+		"other": ""
+	},{
+		"lang1": "How are you?",
+		"lang2": "nei5 hou2 ma3?",
+		"other": ""
+	},{
+		"lang1": "Fine",
+		"lang2": "gei2 hou2",
+		"other": ""
+	},{
+		"lang1": "I don't understand",
+		"lang2": "ngo5 tlang1g1 m4 ming4",
+		"other": ""
+	},{
+		"lang1": "It doesn't matter",
+		"lang2": "m4 gan2 jiu3",
+		"other": ""
+	},{
+		"lang1": "Cantonese",
+		"lang2": "gwong2 dung1 waa2",
+		"other": ""
+	},{
+		"lang1": "he / she",
+		"lang2": "keoi5",
+		"other": ""
+	},{
+		"lang1": "we / us",
+		"lang2": "ngo5 dei6",
+		"other": ""
+	},{
+		"lang1": "you (plural)",
+		"lang2": "nei5 dei6",
+		"other": ""
+	},{
+		"lang1": "they",
+		"lang2": "keoi5 dei6",
+		"other": ""
+	},{
+		"lang1": "my (possessive)",
+		"lang2": "ngo5 ge3",
+		"other": ""
+	},{
+		"lang1": "your (possessive)",
+		"lang2": "nei5 ge3",
+		"other": ""
+	},{
+		"lang1": "his / her (possessive)",
+		"lang2": "keoi5 ge3",
+		"other": ""
+	},{
+		"lang1": "our (possessive)",
+		"lang2": "ngo5 dei6 ge3",
+		"other": ""
+	},{
+		"lang1": "your (plural + possessive)",
+		"lang2": "nei5 dei6 ge3",
+		"other": ""
+	},{
+		"lang1": "their (possessive)",
+		"lang2": "keoi5 dei6 ge3",
+		"other": ""
+	},{
+		"lang1": "0",
+		"lang2": "ling4",
+		"other": ""
+	},{
+		"lang1": "1",
+		"lang2": "jat1",
+		"other": ""
+	},{
+		"lang1": "2",
+		"lang2": "ji6",
+		"other": ""
+	},{
+		"lang1": "3",
+		"lang2": "saam1",
+		"other": ""
+	},{
+		"lang1": "4",
+		"lang2": "sei3",
+		"other": ""
+	},{
+		"lang1": "5",
+		"lang2": "ng5",
+		"other": ""
+	},{
+		"lang1": "6",
+		"lang2": "luk6",
+		"other": ""
+	},{
+		"lang1": "7",
+		"lang2": "cat1",
+		"other": ""
+	},{
+		"lang1": "8",
+		"lang2": "baat3",
+		"other": ""
+	},{
+		"lang1": "9",
+		"lang2": "gau2",
+		"other": ""
+	},{
+		"lang1": "10",
+		"lang2": "sap6",
+		"other": ""
+	},{
+		"lang1": "mobile phone",
+		"lang2": "sau2 gei3",
+		"other": ""
+	},{
+		"lang1": "how many",
+		"lang2": "gei2 do1",
+		"other": ""
+	},{
+		"lang1": "perhaps",
+		"lang2": "bat1 jyu4",
+		"other": ""
+	},{
+		"lang1": "phone",
+		"lang2": "din6 waa2",
+		"other": ""
+	},{
+		"lang1": "Okay",
+		"lang2": "hou2 aa3",
+		"other": ""
+	},{
+		"lang1": "How much is it?",
+		"lang2": "gei2 do1 shin3 aa3?",
+		"other": ""
+	}
+]
